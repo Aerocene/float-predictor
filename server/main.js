da8d53fd0855dc2968b9af6e7049fa1fb09a1488
i-n-g-o <ingo.randolf@servus.at>
2019-08-15 18:15:03 +0200

    safety: check for valid email in settings

--- server/main.js
+++ server/main.js
@@ -10,15 +10,18 @@ Accounts.onCreateUser((options, user) => {
 
   if (user && user.emails && user.emails[0] && user.emails[0].address) {
 
-    Email.send({ 
-      to: user.emails[0].address, 
-      from: Meteor.settings.emails.support, 
-      subject: "Welcome to Floatpredictor", 
-      text: "Your account for Floatpredictor was created successfully. Aerocene."
-    });
+    // check if we have emails in our settings
+    if (Meteor.settings.emails !== undefined) {
 
+      Email.send({ 
+        to: user.emails[0].address, 
+        from: Meteor.settings.emails.support, 
+        subject: "Welcome to Floatpredictor", 
+        text: "Your account for Floatpredictor was created successfully. Aerocene."
+      });
+
+    }
   }
-
 
   return user;
 });
