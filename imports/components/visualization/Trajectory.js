3e780d9d7deeded748c7765d5148e1afd8c2ce37
i-n-g-o <ingo.randolf@servus.at>
2019-08-22 00:23:49 +0200

    align Tajectory with ExplorerTajectory-data

--- imports/components/visualization/Trajectory.js
+++ imports/components/visualization/Trajectory.js
@@ -19,15 +19,11 @@ class Trajectory {
         const departureDate = new Date(viz.startingDate);// this.valueOf()
         departureDate.setDate(departureDate.getDate() + viz.minTrack);
 
-        this.departure = {
-            city: departure.city,
-            country: departure.country,
-            coordinates: {
-                latitude: departure.lat,
-                longitude: departure.lng,
-            },
-        };
-        
+        this.departure_city = departure.city;
+        this.departure_country = departure.country;
+        this.departure_latitude = departure.lat;
+        this.departure_longitude = departure.lng;
+
         this.min_dist = viz.minDist;
         this.min_time = this.minTime;
         this.departure_date = departureDate.toISOString();
@@ -37,20 +33,19 @@ class Trajectory {
         this.path = data;
         this.svg = svg;
         this.svgB64 = "data:image/svg+xml;base64," + btoa(svg);
-        this.explorerIndex = viz.minTrack;
+        //this.explorerIndex = viz.minTrack;
 
         if (viz.flightType === 'planned') {
-            this.destination = {
-              city: destination.city,
-              country: destination.country,
-              coordinates: {
-                latitude: destination.lat,
-                longitude: destination.lng,
-              },
-            };
+            this.destination_city = destination.city;
+            this.destination_country = destination.country;
+            this.destination_latitude = destination.lat;
+            this.destination_longitude = destination.lng;
         } else {
             this.destination = undefined;
         }
+
+        this.email = null;
+        this.ip = null;
     }
 }
 
