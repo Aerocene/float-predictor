fe7b4874ab2f1286a740a4730e74fd8dc696793a
Ingo Randolf <ingo.randolf@servus.at>
2019-12-26 12:35:35 +0100

    remove code from server

--- server/main.js
+++ server/main.js
@@ -97,27 +97,8 @@ Meteor.methods({
   listDir() {
     return doListDir();
   },
-  traj4multi2d(day, pressure, destLat, destLong, urls) {
-
-    // log gfs.time
-    console.log("trajectory (" + day + ") on data from: " + runCommand("cat", [process.env.GFS_NPZ_DATA + "/gfs.time"]));    
-
-    // var start = new Date().getTime();
-
-    const result = runCommand("python", [process.env.TRAJ_4_MULTI_2D_SCRIPT, day, pressure, destLat, destLong, urls]);
-
-    // var end = new Date().getTime();
-    // console.log("traj4multi2d: " + (end - start) + " [ms]");
-    
-    return result;
-  }
 });
 
-
-
-
-var gfs08;
-var gfs20;
 
 Meteor.startup(() => {
   
@@ -128,108 +109,6 @@ Meteor.startup(() => {
   console.log("------------------------------------");
   console.log("starting up: " + Meteor.settings.public.mode);
   console.log(runCommand("date"));
-  console.log("python: " + runCommand("which", ["python"]));
-  console.log("python version: " + runCommand("python", ["--version"]));
-  console.log("");
-
-  console.log("------------------------------------");
-  console.log(runCommand("find", ["/app/bundle/programs/web*", "-type", "d"]));
-  console.log("------------------------------------");
-  console.log(runCommand("ls", ["-l", "/app/bundle/programs/web.browser/app/data/gfs/"]));
-  console.log(runCommand("ls", ["-l", "/app/bundle/programs/web.browser/app/data/gfs/data/"]));
-  console.log(runCommand("ls", ["-l", "/app/bundle/programs/web.browser/app/data/gfs/data/10/"]));
-  
-  // make sure script is executable
-  runCommand("chmod", ["a+x", Assets.absoluteFilePath("download_gfs.sh")]);
-
-  if (Meteor.settings.public.mode !== undefined &&
-    Meteor.settings.public.mode !== "local") {
-
-      // initially remove lock file
-      runCommand("rm", [process.env.GFS_SCRIPT_PATH + "/.lock"]);
-    
-      // install cron-jobs
-      gfs08 = new Cron(function() {
-
-        runCommand(Assets.absoluteFilePath("download_gfs.sh"), undefined, (msg) => {
-          console.log("downloadGfsCron: " + msg);        
-        }, (error) => {
-          console.error("downloadGfsCron: " + error);        
-        });
-      
-      }, {
-        minute: 10,
-        hour: 8
-      });
-      
-      gfs20 = new Cron(function() {
-        
-        runCommand(Assets.absoluteFilePath("download_gfs.sh"), undefined, (msg) => {
-          console.log("downloadGfsCron: " + msg);        
-        }, (error) => {
-          console.error("downloadGfsCron: " + error);        
-        });
-      
-      }, {
-        minute: 10,
-        hour: 20
-      });
-
-      // run update
-      runCommandAsnc(
-        Assets.absoluteFilePath("download_gfs.sh"), 
-        undefined, 
-        (msg) => {
-          console.log("downloadGfsAsync: " + msg);        
-        }, 
-        (error) => {
-          console.error("downloadGfsAsync: " + error);        
-        }
-      );    
-
-  } else {
-    console.log("not starting downloadGfs");    
-  }
-
-  
-  
-  // console.log("GFS_NPZ_DATA: " + process.env.GFS_NPZ_DATA);
-  // console.log("TRAJ_4_MULTI_2D_SCRIPT: " + process.env.TRAJ_4_MULTI_2D_SCRIPT);
-  
-  
-  // serve files in GFS_NPZ_DATA
-  //
-  // const fs = Npm.require('fs');
-  // 
-  // Picker.route('/scripts/traj4multi2d.php', function (params, req, res, next) {
-  //   //
-  //   console.log("traj4mult: " + JSON.stringify(params));    
-  //   res.end("none");
-  // });
-
-  // Picker.route('/scripts/:_file', function (params, req, res, next) {
-  //   var data = fs.readFileSync(process.env.GFS_NPZ_DATA + "/" + params._file);    
-  //   res.write(data);
-  //   res.end();
-  // });
-
-  Picker.route('/log/ps/', function (params, req, res, next) {
-    
-    console.log("----- ps ----");  
-    console.log(runCommand("ps", ["-o", "pid,comm,nice,%cpu,%mem"]));
-
-    res.end();
-  });
-
-  Picker.route('/log/gfsdata/', function (params, req, res, next) {
-    
-    console.log("----- ls gfs data ----");
-    doListDir("/app/bundle/programs/web.browser/app/data/gfs/data/10/");
-    doListDir("/app/bundle/programs/web.browser.legacy/app/data/gfs/data/10/");
-
-    res.end();
-  });
-
 });
 
 
