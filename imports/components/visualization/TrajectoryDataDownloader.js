0abf3aaf8fa7f7d4a671d5cb9af0d5decf7aefa6
Ingo Randolf <ingo.randolf@servus.at>
2019-12-26 12:34:22 +0100

    use FloatProfile, correct errors in Downloader

--- imports/components/visualization/TrajectoryDataDownloader.js
+++ imports/components/visualization/TrajectoryDataDownloader.js
@@ -7,6 +7,7 @@
 */
 
 import moment from 'moment';
+import FloatProfile from './FloatProfile';
 
 const MAX_ERRORS = 10;
 
@@ -32,7 +33,7 @@ class TrajectoryDataDownloader {
 
   retryWithError(error, date, day, departure, destination, altitude)
   {
-    console.error(`retry with errors (${this.errors}/${MAX_ERRORS}): ${error}`);
+    console.error(`retry with errors (${this.errors}/${MAX_ERRORS}): ${JSON.stringify(error)}`);
     
     if (this.errors > MAX_ERRORS)
     {
@@ -45,23 +46,33 @@ class TrajectoryDataDownloader {
     else
     {
       this.errors++;
-      downloadMultiS(date, day, departure, destination, altitude);
+      this.downloadMultiS(date, day, departure, destination, altitude);
     }
   }
 
   downloadMultiS(date, day, departure, destination, altitude)
   {
     const start_datetime = `${moment(date).format(escape('YYYY-MM-DDTHH'))}%3A00%3A00%2B01%3A00`;
+
+    const mir = new FloatProfile("mir", "up_down_profile", 3.6, altitude, 1.3, 0, 2.5);
+    const explorer = new FloatProfile("explorer", "up_down_profile", 1.5, altitude, 1.29, 3 * 3600, 2.5);
+    const generic_float = new FloatProfile("generic", "float_profile", 3.6, altitude, 1.3, 0, 2.5);
+
+    const float = generic_float;
+
     const lat = departure.lat;
     const lng = departure.lng < 0 ? 360+departure.lng : departure.lng;
     const time_resolution = 4000; //1200
-    const ascent_rate = 5;
-    const float_profile = "float_profile";
-    const use_sunrise = 1;
+    const use_sunrise = 0;
     const launch_altitude = 0;
 
-    const url = `https://predict.aerocene.org/api/v1/?profile=${float_profile}&time_resolution=${time_resolution}&use_sunrise=${use_sunrise}&offset_days=${day}&launch_latitude=${lat}&launch_longitude=${lng}&launch_altitude=${launch_altitude}&launch_datetime=${start_datetime}&ascent_rate=${ascent_rate}&float_altitude=${altitude}`;
-    // console.log("url: " + url);
+    let url = `https://predict.aerocene.org/api/v1/?profile=${float.profile}&time_resolution=${time_resolution}&use_sunrise=${use_sunrise}&offset_days=${day}&launch_latitude=${lat}&launch_longitude=${lng}&launch_altitude=${launch_altitude}&launch_datetime=${start_datetime}&ascent_rate=${float.ascent_rate}&float_altitude=${float.float_altitude}`;
+    console.log("url: " + url);
+
+    if (float.profile === "up_down_profile")
+    {
+      url += `&descent_rate=${float.descent_rate}&descent_before_sunset=${float.descent_before_sunset}`;
+    }
 
     fetch(url).then(
       response => response.json()
@@ -70,14 +81,14 @@ class TrajectoryDataDownloader {
       // console.log(jsonData);
 
       if (jsonData === undefined) {
-        retryWithError("no jsondata", day, departure, destination, altitude);
+        this.retryWithError("no jsondata", date, day, departure, destination, altitude);
         return;
       }
 
       // check if this is an error object
       if (jsonData.error !== undefined)
       {
-        retryWithError("did not get trajectory data for: " + day, day, departure, destination, altitude);
+        this.retryWithError(jsonData.error, date, day, departure, destination, altitude);
         return;
       }
 
@@ -97,7 +108,7 @@ class TrajectoryDataDownloader {
 
     }).catch((r) => {
       console.error(`Trajectory Downloader error (${day})`);
-      retryWithError(r, day, departure, destination, altitude);
+      this.retryWithError(r, date, day, departure, destination, altitude);
     });
 
   }
