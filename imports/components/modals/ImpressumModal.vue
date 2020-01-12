b6dabbb8089021b0b81b093f6d413f53995f2bb2
Ingo Randolf <ingo.randolf@servus.at>
2020-01-12 20:17:15 +0100

    restyle impressum and disclaimer, add forgotten Instruction.vue

--- imports/components/modals/ImpressumModal.vue
+++ imports/components/modals/ImpressumModal.vue
@@ -1,34 +1,51 @@
 <template>
     <!-- Modal Component -->
-    <b-modal id="impressum-modal" class="page-modal" centered hide-footer size="lg"
-             title="Impressum">
-        <i slot="modal-header-close" class="fp fp-close"></i>
-        <div class="entry-body">
-            <p>
-            The Aerocene Float Predictor is being developed by
-            the Aerocene Foundation and the members of its diverse community.
-        </p>
-        <h4>TRAJECTORY COMPUTATION</h4>
-        <p>
-            <a href="https://www.cuspaceflight.com/" target="_blank">
-            Camebridge Space Flight (CUSF)</a> - <a href="https://github.com/cuspaceflight/tawhiri" target="_blank">Tawhiri</a>.
-        </p>
-        <h4>VISUALISATION, DESIGN AND DEVELOPMENT</h4>
-        <p>
-            <a href="http://www.studiofolder.it/" target="_blank">Studio Folder</a> (Marco Ferrari, Elisa Pasqual, Luca Ferrario, Iacopo Leardini)
-            with <a href="http://angelosemeraro.info/" target="_blank">Angelo Semeraro</a> and <a href="https://ingorandolf.info/" target="_blank">Ingo Randolf</a>.
-        </p>
-        <h4>PROJECT DEVELOPMENT AND COORDINATION</h4>
-        <p>
-            <a href="http://aerocene.org/" target="_blank">Aerocene Foundation</a> (Joaquin Ezcurra, Camilla Berggren Lundell, Alice Lampert),
-            <a href="http://tomassaraceno.com/" target="_blank">Studio Tomás Saraceno</a>
-            (Erik Vogler, Saverio Cantoni, Rosalie Laurin, Yelta Köm,
-            Connie Chester, Roxanne Mackie, Sofia Lemos, Zaida Violan),
-            <a href="https://publiclab.org/ " target="_blank">Public Lab</a>
-            (Nick Shapiro, Liz Barry), Imperial College London, Radio Amateurs DE
-            & UK, and the <a href="https://ukhas.org.uk/" target="_blank">UK High Altitude Society</a>.
-        </p></div>
-    </b-modal>
+    <!-- <div @click="close" v-if="showImpressum"> -->
+
+    <div>
+        <b-modal 
+            id="impressum-modal" 
+            class="page-modal"
+            hide-footer 
+            hide-header 
+            no-fade
+            centered
+            size="lg"            
+        >
+            
+            <div class="entry-body" @click="$bvModal.hide('impressum-modal')">
+                <h3 style="margin-top: 0px;">Impressum</h3>
+                <p>
+                    The Aerocene Float Predictor is being developed by
+                    the Aerocene Foundation and the members of its diverse community.
+                </p>
+                <h4>TRAJECTORY COMPUTATION</h4>
+                <p>
+                    <a href="https://www.cuspaceflight.com/" target="_blank">
+                    Camebridge Space Flight (CUSF)</a> - <a href="https://github.com/cuspaceflight/tawhiri" target="_blank">Tawhiri</a>.
+                </p>
+                <h4>VISUALISATION, DESIGN AND DEVELOPMENT</h4>
+                <p>
+                    <a href="http://www.studiofolder.it/" target="_blank">Studio Folder</a> (Marco Ferrari, Elisa Pasqual, Luca Ferrario, Iacopo Leardini).
+                    with <a href="http://angelosemeraro.info/" target="_blank">Angelo Semeraro</a>.<br/>
+                    <a href="https://ingorandolf.info/" target="_blank">Ingo Randolf</a><br/>
+                    <a href="http://www.roham.eu" target="_blank">Roham Fayazi</a>
+                </p>
+                <h4>PROJECT DEVELOPMENT AND COORDINATION</h4>
+                <p>
+                    <a href="http://aerocene.org/" target="_blank">Aerocene Foundation</a> (Joaquin Ezcurra, Camilla Berggren Lundell, Alice Lampert),
+                    <a href="http://tomassaraceno.com/" target="_blank">Studio Tomás Saraceno</a>
+                    (Erik Vogler, Saverio Cantoni, Rosalie Laurin, Yelta Köm,
+                    Connie Chester, Roxanne Mackie, Sofia Lemos, Zaida Violan),
+                    <a href="https://publiclab.org/ " target="_blank">Public Lab</a>
+                    (Nick Shapiro, Liz Barry), Imperial College London, Radio Amateurs DE
+                    & UK, and the <a href="https://ukhas.org.uk/" target="_blank">UK High Altitude Society</a>.
+                </p>
+            </div>
+
+        </b-modal>
+    </div>
+    <!-- </div> -->
 </template>
 
 <script>
@@ -39,6 +56,12 @@
 */
 export default {
   name: 'impressum-modal',
+  methods: {
+      closeModal() {
+          console.log("close");    
+        //   this.$refs.impressum.close();    
+      }
+  },
 };
 </script>
 
@@ -49,5 +72,9 @@ export default {
     .modal-body {
         font-size: 1.2em;
     }
+}
+.modal-dialog-centered {
+    height: calc(100% - 75px);
+    padding-top:75px;
 }
 </style>
---
+++ imports/components/pages/Instructions.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="main-content-local">
+        <f-slider />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.main-content-local {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  padding-top: 65px;
+  z-index: 20;
+
+  padding-left: 2em;
+  padding-right: 2em;
+
+  background-color: rgba(0, 0, 0, 1);
+
+}
+
+
+</style>
+
+<script>
+/**
+ * @author Studio Folder - @StudioFolder / http://studiofolder.it
+ * @author Iacopo Leardini - @iacopolea
+ * @author Angelo Semeraro - @angeloseme / http://angelosemeraro.info
+*/
+import backToViz from '../parts/BackToViz';
+import fSlider from '../slider/FSlider';
+
+export default {
+  name: 'how-to-float',
+  components: {
+    backToViz, fSlider
+  },
+  data() {
+    return {
+      upperHeight: 0,
+      imgHeight: 0,
+      figureHeight: 0,
+      captionWidth: 0,
+    };
+  },
+  computed: {
+  },
+  created() {
+    this.$store.commit('flightSimulator/setVisualizationState', 6);
+    if (window.matchMedia('(max-width: 424px)').matches) {
+      this.imgHeight = '150px';
+      this.figureHeight = '333px';
+      this.captionWidth = '267px';
+    } else if (window.matchMedia('(max-width: 767px)').matches) {
+      this.imgHeight = '200px';
+      this.figureHeight = '433px';
+      this.captionWidth = '356px';
+    } else {
+      this.imgHeight = '378px';
+      this.figureHeight = '789px';
+      this.captionWidth = '672px';
+    }
+  },
+  mounted() {
+    // this.upperHeight = `${this.$refs.content.clientHeight}px`;
+  },
+};
+</script>
