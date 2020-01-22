be732af02ecb6c20cd3760bc6f77fe370221fda6
Ingo Randolf <ingo.randolf@servus.at>
2020-01-22 17:18:34 +0100

    adjust impressum

--- imports/components/modals/ImpressumModal.vue
+++ imports/components/modals/ImpressumModal.vue
@@ -16,30 +16,25 @@
             <div class="entry-body" @click="$bvModal.hide('impressum-modal')">
                 <h3 style="margin-top: 0px;">Impressum</h3>
                 <p>
-                    The Aerocene Float Predictor is being developed by
+                    The Aerocene Float Predictor is being developed by 
                     the Aerocene Foundation and the members of its diverse community.
                 </p>
                 <h4>TRAJECTORY COMPUTATION</h4>
                 <p>
-                    <a href="https://www.cuspaceflight.com/" target="_blank">
-                    Camebridge Space Flight (CUSF)</a> - <a href="https://github.com/cuspaceflight/tawhiri" target="_blank">Tawhiri</a>.
+                    <a href="https://eapsweb.mit.edu/" target="_blank">MIT Department of Earth, Atmospheric and Planetary Sciences</a> (Glenn Flierl, Lodovica Illari, Bill McKenna) and
+                    <a href="https://www.cuspaceflight.com/" target="_blank">Cambridge University Space Flight (CUSF)</a>.
                 </p>
                 <h4>VISUALISATION, DESIGN AND DEVELOPMENT</h4>
                 <p>
-                    <a href="http://www.studiofolder.it/" target="_blank">Studio Folder</a> (Marco Ferrari, Elisa Pasqual, Luca Ferrario, Iacopo Leardini).
-                    with <a href="http://angelosemeraro.info/" target="_blank">Angelo Semeraro</a>.<br/>
-                    <a href="https://ingorandolf.info/" target="_blank">Ingo Randolf</a><br/>
-                    <a href="http://www.roham.eu" target="_blank">Roham Fayazi</a>
+                    Ingo Randolf, Roham Fayazy, Studio Folder, Angelo Semeraro.                    
                 </p>
                 <h4>PROJECT DEVELOPMENT AND COORDINATION</h4>
                 <p>
-                    <a href="http://aerocene.org/" target="_blank">Aerocene Foundation</a> (Joaquin Ezcurra, Camilla Berggren Lundell, Alice Lampert),
-                    <a href="http://tomassaraceno.com/" target="_blank">Studio Tomás Saraceno</a>
-                    (Erik Vogler, Saverio Cantoni, Rosalie Laurin, Yelta Köm,
-                    Connie Chester, Roxanne Mackie, Sofia Lemos, Zaida Violan),
-                    <a href="https://publiclab.org/ " target="_blank">Public Lab</a>
-                    (Nick Shapiro, Liz Barry), Imperial College London, Radio Amateurs DE
-                    & UK, and the <a href="https://ukhas.org.uk/" target="_blank">UK High Altitude Society</a>.
+                    <a href="http://aerocene.org/" target="_blank">Aerocene Foundation</a>, 
+                    <a href="http://tomassaraceno.com/" target="_blank">Studio Tomás Saraceno</a>, 
+                    <a href="https://publiclab.org/" target="_blank">Public Lab</a>, 
+                    Imperial College London, Radio Amateurs DE &amp; UK, 
+                    and the <a href="https://ukhas.org.uk/" target="_blank">UK High Altitude Society</a>.
                 </p>
             </div>
 
